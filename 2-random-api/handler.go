1b46abbdb45043e0d9946a39259c518048e0a747
Yuriy <39434769+YuraYuzgin@users.noreply.github.com>
2025-07-21 19:33:44 +0300

    Исправлено дз 2 повторно

--- 2-random-api/handler.go
+++ 2-random-api/handler.go
@@ -15,6 +15,6 @@ func NewRandomNumberHandler(router *http.ServeMux) {
 
 func (hanler *RandomNumberHandler) CreateRandomNum() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte(fmt.Sprint(rand.Intn(7))))
+		w.Write([]byte(fmt.Sprint(1 + rand.Intn(6))))
 	}
 }
